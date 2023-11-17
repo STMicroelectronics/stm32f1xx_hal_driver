39f8335b2ccfb371707e25571859c268776945ce
Rania JMAI <rania.jmai@st.com>
2023-11-17 09:26:56 +0100

    [HAL][I2C] Update the HAL I2C driver to reset PreviousState to I2C_STATE_NONE at the end of transfer

--- Src/stm32f1xx_hal_i2c.c
+++ Src/stm32f1xx_hal_i2c.c
@@ -5627,7 +5627,9 @@ static void I2C_MasterReceive_RXNE(I2C_HandleTypeDef *hi2c)
   if (hi2c->State == HAL_I2C_STATE_BUSY_RX)
   {
     uint32_t tmp;
+    uint32_t CurrentXferOptions;
 
+    CurrentXferOptions = hi2c->XferOptions;
     tmp = hi2c->XferCount;
     if (tmp > 3U)
     {
@@ -5683,7 +5685,14 @@ static void I2C_MasterReceive_RXNE(I2C_HandleTypeDef *hi2c)
         else
         {
           hi2c->Mode = HAL_I2C_MODE_NONE;
-          hi2c->PreviousState = I2C_STATE_MASTER_BUSY_RX;
+          if ((CurrentXferOptions == I2C_FIRST_AND_LAST_FRAME) || (CurrentXferOptions == I2C_LAST_FRAME))
+          {
+            hi2c->PreviousState = I2C_STATE_NONE;
+          }
+          else
+          {
+            hi2c->PreviousState = I2C_STATE_MASTER_BUSY_RX;
+          }
 
 #if (USE_HAL_I2C_REGISTER_CALLBACKS == 1)
           hi2c->MasterRxCpltCallback(hi2c);
@@ -5831,7 +5840,14 @@ static void I2C_MasterReceive_BTF(I2C_HandleTypeDef *hi2c)
     else
     {
       hi2c->Mode = HAL_I2C_MODE_NONE;
-      hi2c->PreviousState = I2C_STATE_MASTER_BUSY_RX;
+      if ((CurrentXferOptions == I2C_FIRST_AND_LAST_FRAME) || (CurrentXferOptions == I2C_LAST_FRAME))
+      {
+        hi2c->PreviousState = I2C_STATE_NONE;
+      }
+      else
+      {
+        hi2c->PreviousState = I2C_STATE_MASTER_BUSY_RX;
+      }
 #if (USE_HAL_I2C_REGISTER_CALLBACKS == 1)
       hi2c->MasterRxCpltCallback(hi2c);
 #else
@@ -7142,7 +7158,14 @@ static void I2C_DMAXferCplt(DMA_HandleTypeDef *hdma)
       else
       {
         hi2c->Mode = HAL_I2C_MODE_NONE;
-        hi2c->PreviousState = I2C_STATE_MASTER_BUSY_RX;
+        if ((CurrentXferOptions == I2C_FIRST_AND_LAST_FRAME) || (CurrentXferOptions == I2C_LAST_FRAME))
+        {
+          hi2c->PreviousState = I2C_STATE_NONE;
+        }
+        else
+        {
+          hi2c->PreviousState = I2C_STATE_MASTER_BUSY_RX;
+        }
 
 #if (USE_HAL_I2C_REGISTER_CALLBACKS == 1)
         hi2c->MasterRxCpltCallback(hi2c);
